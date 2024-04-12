5b71685479b02f069eb44b3d68a68d0b01da8df2
Kavan <kavangami13@gmail.com>
2024-04-12 15:52:58 +0530

    done

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+var typed = new Typed(".auto-type", {
+    strings: ["Web Developer", "UI/UX Designer", "Front-End Developer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+})
+AOS.init({
+    offset: 50,
+    duration: 1000,
+    once: true
+});
